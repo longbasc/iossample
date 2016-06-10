ad865c9b43d6418bdce209a91d27c5f0fd89ef74
Nguyen Long <long.nguyen@apac-consulting.net>
2016-06-10 14:55:43 -0700

    Initial Commit

---
+++ TestSingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSingleView
+//
+//  Created by admin on 6/10/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
